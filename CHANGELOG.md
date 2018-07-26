cdcc3b2252f7b6332a43f405981532b2666440a4
m1v1m2v2 <31479699+m1v1m2v2@users.noreply.github.com>
2018-07-26 16:24:00 -0700

    firstcommit

---
+++ CHANGELOG.md
@@ -0,0 +1,74 @@
+This CHANGELOG.md file will contain the update log for the latest set of updates to the templates
+
+
+# UPDATES for Master (Release 3.9) - July 20, 2018
+
+1.  Added support for Private Network
+2.  Removed inbound NAT rules for master LB to better secure master nodes
+
+
+# UPDATES for Master (Release 3.9) - July 14, 2018
+
+1.  Added support for Accelerated Networking
+2.  Added support for existing or new VNet
+
+
+# UPDATES for Master (Release 3.9) - May 22, 2018
+
+1.  Added parameter for CNS VM Size
+2.  Added support for non-HA masters by allowing a single master
+3.  Cleaned up Azure Cloud Provider configuration
+
+
+# UPDATES for Master (Release 3.9) - May 19, 2018
+
+1.  Updated scripts to support 3.9.27
+2.  Added Support for RHEL 7.5
+3.  Added Container Native Storage (CNS) support
+4.  Added support for custom IP range for the Virtual Network
+
+# UPDATES for Master (Release 3.9) - March 28, 2018
+
+1.  Create Release 3.9 Branch
+2.  Updating scripts for 3.9 repository
+3.  Switch to port 443 for web console
+4.  Remove old unused resources
+
+
+# UPDATES for Master (Release 3.7) - February 14, 2018
+
+1.  Created Release 3.7 Branch
+2.  Update deployOpenShift.sh file to separate out Ansible Playbooks
+3.  Created separate repo for OpenShift installation Playbooks
+
+
+# UPDATES for Release 3.7 - January 12, 2018
+
+1.  Inject the Private Key into Bastion host during prep.
+2.  Add support for managed and unmanaged disks.
+3.  Update prep script to simplify Cloud Access registration for username/password or activation key/organization id.
+4.  Update Azure Cloud Provider playbooks - no need to delete node and include cluster reboot.
+5.  Include additional data disk sizes.
+6.  Create storage class based on managed or unmanaged disk.
+7.  General cleanup.
+
+
+# UPDATES for Release 3.6 - September 29, 2017
+
+1.  Removed installation of Azure CLI as this is no longer needed.
+2.  Removed dnslabel parameters and made them variables to simplify deployment.
+3.  Added new D2-64v3, D2s-64sv3, E2-64v3, and E2s-64sv3 VM types.
+4.  Updated prep scripts to include additional documented pre-requisites.
+5.  Set OS disk size to 64 GB and updated prep scripts to expand root partition.
+6.  Removed option to install single master cluster.  Now supports 3 or 5 Masters and 2 or 3 Infra nodes.
+7.  Configure RHEL to use NetworkManager on eth0.
+8.  Added additional troubleshooting for Azure Cloud Configuration playbooks (Exit Codes 7 - 10).
+9.  Updated to latest versions of APIs - includes reworking of Storage Account creation.
+10. Bastion Host - separate Storage Account and VM size definition.
+11. Enabled Diagnostics Storage for all VMs.
+12. Added Tags to all resources.
+13. Switched to nip.io (versus xip.io).
+14. Added option to enable Azure Cloud Provider (true or false).
+15. Moved Metric and Logging setup to post cluster install.
+16. General cleanup (removed unnecessary resources, variables, etc.).
+
---
+++ scripts/bastionPrep.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+echo $(date) " - Starting Bastion Prep Script"
+
+USERNAME_ORG=$1
+PASSWORD_ACT_KEY="$2"
+POOL_ID=$3
+PRIVATEKEY=$4
+SUDOUSER=$5
+
+# Generate private keys for use by Ansible
+echo $(date) " - Generating Private keys for use by Ansible for OpenShift Installation"
+
+runuser -l $SUDOUSER -c "echo \"$PRIVATEKEY\" > ~/.ssh/id_rsa"
+runuser -l $SUDOUSER -c "chmod 600 ~/.ssh/id_rsa*"
+
+# Remove RHUI
+
+rm -f /etc/yum.repos.d/rh-cloud.repo
+sleep 10
+
+# Register Host with Cloud Access Subscription
+echo $(date) " - Register host with Cloud Access Subscription"
+
+subscription-manager register --username="$USERNAME_ORG" --password="$PASSWORD_ACT_KEY" || subscription-manager register --activationkey="$PASSWORD_ACT_KEY" --org="$USERNAME_ORG"
+
+if [ $? -eq 0 ]
+then
+    echo "Subscribed successfully"
+elif [ $? -eq 64 ]
+then
+    echo "This system is already registered."
+else
+    echo "Incorrect Username / Password or Organization ID / Activation Key specified"
+    exit 3
+fi
+
+subscription-manager attach --pool=$POOL_ID > attach.log
+if [ $? -eq 0 ]
+then
+    echo "Pool attached successfully"
+else
+    evaluate=$( cut -f 2-5 -d ' ' attach.log )
+    if [[ $evaluate == "unit has already had" ]]
+    then
+        echo "Pool $POOL_ID was already attached and was not attached again."
+    else
+        echo "Incorrect Pool ID or no entitlements available"
+        exit 4
+    fi
+fi
+
+# Disable all repositories and enable only the required ones
+echo $(date) " - Disabling all repositories and enabling only the required repos"
+
+subscription-manager repos --disable="*"
+
+subscription-manager repos \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-ose-3.9-rpms" \
+    --enable="rhel-7-server-ansible-2.4-rpms" \
+    --enable="rhel-7-fast-datapath-rpms" \
+    --enable="rh-gluster-3-client-for-rhel-7-server-rpms"
+
+# Update system to latest packages
+echo $(date) " - Update system to latest packages"
+yum -y update --exclude=WALinuxAgent
+echo $(date) " - System update complete"
+
+# Install base packages and update system to latest packages
+echo $(date) " - Install base packages"
+yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion httpd-tools kexec-tools sos psacct
+yum -y install ansible
+yum -y update glusterfs-fuse
+echo $(date) " - Base package insallation complete"
+
+# Excluders for OpenShift
+yum -y install atomic-openshift-excluder atomic-openshift-docker-excluder
+atomic-openshift-excluder unexclude
+
+# Install OpenShift utilities
+echo $(date) " - Installing OpenShift utilities"
+
+yum -y install atomic-openshift-utils
+echo $(date) " - OpenShift utilities insallation complete"
+
+# Installing Azure CLI
+# From https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-yum
+echo $(date) " - Installing Azure CLI"
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+sudo yum install -y azure-cli
+echo $(date) " - Azure CLI insallation complete"
+
+# Configure DNS so it always has the domain name
+echo $(date) " - Adding DOMAIN to search for resolv.conf"
+echo "DOMAIN=`domainname -d`" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+
+# Run Ansible Playbook to update ansible.cfg file
+echo $(date) " - Updating ansible.cfg file"
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 5 https://raw.githubusercontent.com/microsoft/openshift-container-platform-playbooks/master/updateansiblecfg.yaml
+ansible-playbook -f 10 ./updateansiblecfg.yaml
+
+echo $(date) " - Script Complete"
+
